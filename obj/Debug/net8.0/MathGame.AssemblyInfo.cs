a99b6b68bcaaf563e18c46dc08c72aad9b5755e9
mohamed El-Saeed <mohamedsaead717@gmail.com>
2024-09-16 06:38:12 +0200

    V 2.5

--- obj/Debug/net8.0/MathGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/MathGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MathGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c554b524ffd8fc0e3d51e6b29f59416da16a4d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MathGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MathGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
